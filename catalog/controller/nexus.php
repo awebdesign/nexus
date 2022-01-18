6477729c91a30f83c5912cab7a40a4d9d7caa0f7
D3 <florin@awebdesign.ro>
2022-01-18 14:42:36 +0200

    namespace fix

--- catalog/controller/nexus.php
+++ catalog/controller/nexus.php
@@ -6,8 +6,19 @@ class ControllerNexus extends Controller {
 
     public function index()
     {
-        IArr::get(['foo' => [
-            'bar' => 1
-        ]], 'foo.bar');
+        $arr = [
+            'foo' => [
+                'bar' => 1
+            ]
+        ];
+
+        dump(IArr::get($arr, 'foo.bar'));
+
+        $arr = [
+            ['foo' => 1],
+            ['foo' => 2],
+        ];
+
+        dump(IArr::pluck($arr, 'foo'));
     }
 }
