c2b906d1f83da4cb5a71b444db55139226253877
DaRock <mariuz.felea@gmail.com>
2022-02-07 19:07:55 +0200

    Util alerts improvements

--- system/library/aweb/nexus/Support/Helpers/utils.php
+++ system/library/aweb/nexus/Support/Helpers/utils.php
@@ -22,21 +22,26 @@ if (! function_exists('_alerts')) {
     /**
      * Will show a bootstrap alert element containing: success, warning or errors by default
      *
-     * @param string $key "success" | "warning" | "errors"
+     * @param string $key "success" | "warning" | "errors" | if is null show all
      * @return void
      */
-    function _alerts(string $key = 'errors', $default)
-    {;
-        $alerts = (array) Session::get('_'.$key, $default);
-        if ($alerts) {
-            $class = $key === 'errors' ? 'danger' : $key;
-            echo '<div class="alert alert-'.$class.'">
-                <ul>';
-                foreach ($alerts as $a) {
-                    echo "<li>$a</li>";
-                }
-            echo '</ul>
-            </div>';
+    function _alerts(string $key = null, $default = null)
+    {
+        $keys = is_null($key) ? ['success', 'warning', 'errors'] : [$key];
+
+        foreach($keys as $key)
+        {
+            $alerts = (array) Session::get('_'.$key, $default);
+            if ($alerts) {
+                $class = $key === 'errors' ? 'danger' : $key;
+                echo '<div class="alert alert-'.$class.'">
+                    <ul>';
+                    foreach ($alerts as $a) {
+                        echo "<li>$a</li>";
+                    }
+                echo '</ul>
+                </div>';
+            }
         }
     }
 }
